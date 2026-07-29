e951f3147fde01ddd7c07d92faa379eccf90030e
vikneshwaran-rk <vikneshwaran.rk.2025.cse@rajalakshmi.edu.in>
2026-07-29 22:01:09 +0530

    Add q17.java

---
+++ for loop problems/q17.java
@@ -0,0 +1,11 @@
+//Print even number between 1 to 10 Using for loop and if else.
+
+class q17{
+    public static void main(String arg[]){
+        for(int i=1; i<=10; i=i+1){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
